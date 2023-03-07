f26b681dcbdc296ccba46ecd189fca4465f9f923
Saman <s.sadeghy325@gmail.com>
2023-03-07 00:07:30 -0600

    First page template

---
+++ index.js
@@ -0,0 +1,23 @@
+import path from 'path';
+import {fileURLToPath} from 'url';
+import express from 'express';
+
+
+const __filename = fileURLToPath(import.meta.url); // or process.resolve()
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static(__dirname));
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ion-3d-engine.html'));
+});
+
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
